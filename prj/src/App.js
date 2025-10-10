6405638c1a4050a98d73e639fe751b9ff6e56add
kdhstack <kdhstack@gmail.com>
2025-10-10 21:54:19 +0900

    [Add] Added header to the page

--- prj/src/App.js
+++ prj/src/App.js
@@ -3,9 +3,23 @@ import './App.css';
 
 import shoeImg from './assets/shoe1.png'; 
 
+function Header() {
+  return (
+    <header>
+      <h1>ShoeKing</h1>
+      <nav>
+        <div>
+          장바구니
+        </div>
+      </nav>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <div className="item">
         <img src={shoeImg} alt="신발 이미지" className="itembox"></img> 
       </div>

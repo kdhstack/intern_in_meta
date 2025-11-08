6b460ce7c8014486b1b8c0b20a49e93c2f5af593
kdhstack <kdhstack@gmail.com>
2025-11-08 14:48:40 +0900

    [fix] App.test.js 수정

--- prj/src/App.test.js
+++ prj/src/App.test.js
@@ -1,13 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import {RecoilRoot} from 'recoil';
 import App from './App';
 
-test('이미지 요소 확인', () => {
+
+test('이미지 요소 확인', async () => {
   // 1. App 컴포넌트 렌더링
-  render(<App />);
+  render(
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>);
 
-  // 2. 'alt' 텍스트를 사용하여 이미지 요소를 찾음
-  const imageElement = screen.getByAltText(/신발 이미지/i);
+  // 2. 모킹 서버(db.json)의 첫 번째 상품의 제목을 사용
+  const firstProductTitle = await screen.findByText(/브랜드A - 모델 1/i, {}, {timeout: 4000});
 
-  // 3. 찾은 이미지 요소가 원하는 클래스를 가지고 있는지 확인
-  expect(imageElement).toHaveClass('itembox');
+  // 3. 데이터가 성공적으로 로드되고 렌더링되었는지 확인
+  expect(firstProductTitle).toBeInTheDocument();
 });
